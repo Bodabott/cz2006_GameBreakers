176658645f83c046b358d99c5dbee9d55811333e
jonathan12352 <37117994+jonathan12352@users.noreply.github.com>
2018-03-17 23:50:56 +0800

    changes to manifest

--- app/src/main/java/com/example/gamebreakers/user_ui.java
+++ app/src/main/java/com/example/gamebreakers/user_ui.java
@@ -1,10 +1,14 @@
 package com.example.gamebreakers;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import com.example.gamebreakers.login.Activity_Main;
 
 public class user_ui extends AppCompatActivity {
 
@@ -12,8 +16,11 @@ public class user_ui extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_ui);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    }
 
+    public void clickBrowse(View v)
+    {
+        FragmentManager fragman = getSupportFragmentManager();
+        //fragman.beginTransaction().add(new )
     }
 }
