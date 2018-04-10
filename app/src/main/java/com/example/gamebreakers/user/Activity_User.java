ba55cfe744517b1a744778f1baeafc64f40b23ef
ktan1887979 <ktan18879@users.noreply.github.com>
2018-04-10 16:12:04 +0800

    Revert "search for food + more"
    This reverts commit 3a4afae11bce82da28b5da8d753aa3ed32f4baaa.

--- app/src/main/java/com/example/gamebreakers/user/Activity_User.java
+++ app/src/main/java/com/example/gamebreakers/user/Activity_User.java
@@ -1,12 +1,9 @@
 package com.example.gamebreakers.user;
 
 import android.app.Activity;
-import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -17,12 +14,8 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -32,7 +25,6 @@ import com.example.gamebreakers.entities.DatabaseHelper;
 import com.example.gamebreakers.entities.Order;
 import com.example.gamebreakers.entities.User;
 import com.example.gamebreakers.login.Activity_Main;
-import com.example.gamebreakers.owner.Activity_Owner;
 
 import static com.example.gamebreakers.login.Activity_Main.PASSWORD;
 import static com.example.gamebreakers.login.Activity_Main.USER_NAME;
@@ -50,8 +42,7 @@ public class Activity_User extends AppCompatActivity
     String stallName, food;
     User user;
     DatabaseHelper myDb;
-    Dialog myDialog;
-    Value val = new Value();
+
     android.support.v4.app.FragmentManager fragman= getSupportFragmentManager();
 
     @Override
@@ -60,8 +51,6 @@ public class Activity_User extends AppCompatActivity
         setContentView(R.layout.activity_user);
 
         myDb = new DatabaseHelper(this);
-
-        myDialog = new Dialog(this);
 
         //set initial fragment (Main Menu)
         fragman.beginTransaction()
@@ -107,103 +96,6 @@ public class Activity_User extends AppCompatActivity
 
     }
 
-    public void ShowTopupPopup(View v) {
-
-        Button buttonCancel;
-        Button buttonConfirm;
-        final EditText mEdit;
-
-        myDialog.setContentView(R.layout.topup_popup);
-
-
-        mEdit = (EditText) myDialog.findViewById(R.id.top_up_value);
-        buttonCancel = (Button) myDialog.findViewById(R.id.cancel_button);
-        buttonConfirm = (Button) myDialog.findViewById(R.id.confirm_button);
-
-        buttonConfirm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String un = user.getName();
-                String txt = mEdit.getText().toString();
-                int bal = Integer.parseInt(txt);
-                int totalbal = myDb.getUserBalance(un) + bal;
-                myDb.updateUserBalance(un, totalbal);
-                invalidateOptionsMenu();
-                myDialog.dismiss();
-            }
-        });
-
-        buttonCancel.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                myDialog.dismiss();
-            }
-        });
-        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        myDialog.show();
-    }
-
-    public void SearchStall(View v) {
-
-        final EditText mSearchField = (EditText) findViewById(R.id.search_field);
-        ImageButton mSearchBtn = (ImageButton) findViewById(R.id.search_button);
-
-        mSearchBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Cursor stall_res = myDb.checkifStallExists(mSearchField.getText().toString());
-                if (stall_res.getCount() == 1) {
-                    stallName = mSearchField.getText().toString();
-                    fragman.beginTransaction()
-                            .replace(R.id.content_main, new Fragment_User_BrowseFood())
-                            .addToBackStack(null)
-                            .commit();
-                }
-            }
-        });
-    }
-
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        String val_ue;
-        int val;
-        String un = user.getName();
-        val = myDb.getUserBalance(un);
-        val_ue = "$" + Integer.toString(val);
-
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.custom_menu, menu);
-        MenuItem item = menu.findItem(R.id.value);
-        Button itemButt = (Button) item.getActionView();
-        if(itemButt != null) {
-            itemButt.setText(val_ue);
-            itemButt.setTextColor(Color.WHITE);
-            itemButt.setBackgroundColor(Color.TRANSPARENT);
-        }
-        return true;
-    }
-
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        menu.clear();
-
-        String val_ue;
-        int val;
-        String un = user.getName();
-        val = myDb.getUserBalance(un);
-        val_ue = "$" + Integer.toString(val);
-
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.custom_menu, menu);
-        MenuItem item = menu.findItem(R.id.value);
-        Button itemButt = (Button) item.getActionView();
-        if(itemButt != null) {
-            itemButt.setText(val_ue);
-            itemButt.setTextColor(Color.WHITE);
-            itemButt.setBackgroundColor(Color.TRANSPARENT);
-        }
-        return super.onPrepareOptionsMenu(menu);
-    }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -228,16 +120,10 @@ public class Activity_User extends AppCompatActivity
         final String password_message = intent.getStringExtra(Activity_Main.PASSWORD);
         // set item as selected to persist highlight
         item.setChecked(true);
-        if(item.toString().matches("Current Orders")) {
+        if(item.toString().matches("Current Orders")){
             // Go to User Current Orders. Item is recorded right after payment.
             fragman.beginTransaction()
                     .replace(R.id.content_main, new Fragment_User_CurrentOrders())
-                    .addToBackStack(null)
-                    .commit();
-        }else if(item.toString().matches("Top Up")) {
-            // Go to Top Up Settings.
-            fragman.beginTransaction()
-                    .replace(R.id.content_main, new Fragment_User_Manage_Payment_Details())
                     .addToBackStack(null)
                     .commit();
         }else if(item.toString().matches("History")){
@@ -348,17 +234,7 @@ public class Activity_User extends AppCompatActivity
 
         String newFoodMessage = foodNameConverter(foodMessage,stallMessage,usernameMessage);
         if(myDb.addOrderArrayData(newFoodMessage,usernameMessage,stallMessage)){
-            int foodprice = myDb.getFoodPrice(foodMessage, stallMessage);
-            int current_bal = myDb.getUserBalance(usernameMessage);
-            if (foodprice > current_bal) {
-                Toast.makeText(getApplicationContext(),"INSUFFICIENT BALANCE",Toast.LENGTH_LONG).show();
-            }
-            else {
-                int bal_left = myDb.getUserBalance(usernameMessage) - foodprice;
-                myDb.updateUserBalance(usernameMessage, bal_left);
-                invalidateOptionsMenu();
-                Toast.makeText(getApplicationContext(), "PAYMENT SUCCESSFUL", Toast.LENGTH_LONG).show();
-            }
+            Toast.makeText(getApplicationContext(),"PAYMENT SUCCESSFUL",Toast.LENGTH_LONG).show();
             setResult(Activity.RESULT_OK);
         }
         else Toast.makeText(getApplicationContext(),"PAYMENT NOT SUCCESSFUL",Toast.LENGTH_LONG).show();
@@ -401,7 +277,6 @@ public class Activity_User extends AppCompatActivity
     }
 
     //================SIMPLE ON CLICK METHODS======================
-
     public void browseStall(View v){
         fragman.beginTransaction()
                 .replace(R.id.content_main, new Fragment_User_BrowseStall())
--- app/src/main/java/com/example/gamebreakers/user/Fragment_User_MainMenu.java
+++ app/src/main/java/com/example/gamebreakers/user/Fragment_User_MainMenu.java
@@ -6,8 +6,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.gamebreakers.R;
@@ -21,7 +19,6 @@ public class Fragment_User_MainMenu extends Fragment {
         Activity_User act = (Activity_User) getActivity();
         TextView text = view.findViewById(R.id.balance);
         text.setText("Balance: $" +act.user.getBalance());
-
         return view;
     }
 }
