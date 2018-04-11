78b0eef42792151d0e15533a9e36e1c405e1d1d6
Ching Jia Chin <jiachin1995@gmail.com>
2018-04-11 14:57:43 +0800

    Revert "Revert "search for food + more""
    This reverts commit ba55cfe744517b1a744778f1baeafc64f40b23ef.

--- app/src/main/java/com/example/gamebreakers/user/Fragment_User_MainMenu.java
+++ app/src/main/java/com/example/gamebreakers/user/Fragment_User_MainMenu.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.gamebreakers.R;
@@ -19,6 +21,7 @@ public class Fragment_User_MainMenu extends Fragment {
         Activity_User act = (Activity_User) getActivity();
         TextView text = view.findViewById(R.id.balance);
         text.setText("Balance: $" +act.user.getBalance());
+
         return view;
     }
 }
